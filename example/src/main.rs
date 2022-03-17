f927c020924e93a5dc3a79d7f59bb4bfead21723
sidit77 <sidit77@gmail.com>
2022-03-17 16:55:15 +0100

    small translation fix

--- example/src/main.rs
+++ example/src/main.rs
@@ -10,14 +10,14 @@ fn main() {
             .rotate(f32::to_radians(30.0)));
 
 
-    let img = rasterize(512, 512, -3.0, sdf);
-    img.save("output1.png").expect("Could not write img");
+    rasterize(512, 512, -3.0, sdf).save("output1.png").expect("Could not write img");
+    rasterize(64, 64, -3.0, sdf).save("output2.png").expect("Could not write img");
 
     let f = Vec2::new(512.0, 512.0) * 0.5;
     let underlay = Shapes::hexagon(0.75).rotate(f32::to_radians(90.0));
     let img2 = GrayImage::from_fn(512, 512, |x, y| {
-        let p = (Vec2::new(x as f32, y as f32) - f) / f;
-        if img.get_pixel(x, y).0[0] > u8::MAX / 2 {
+        let p = (Vec2::new(x as f32, y as f32) + Vec2::new(0.5, 0.5) - f) / f;
+        if sdf.density(p) < 0.0 {
             Luma([255])
         } else if underlay.density(p) < 0.0 {
             Luma([128])
@@ -26,13 +26,13 @@ fn main() {
         }
     });
 
-    img2.save("output2.png").expect("Could not write img");
+    img2.save("output3.png").expect("Could not write img");
 }
 
 fn rasterize(width: u32, height: u32, factor: f32, sdf: impl Sdf) -> GrayImage {
     let f = Vec2::new(width as f32, height as f32) * 0.5;
     GrayImage::from_fn(width, height, |x, y| {
-        let p = (Vec2::new(x as f32, y as f32) - f) / f;
+        let p = (Vec2::new(x as f32, y as f32) + Vec2::new(0.5, 0.5) - f) / f;
         let d = factor * sdf.density(p);
         let h = u8::MAX as f32 * 0.5;
         Luma([(h + d * h).clamp(u8::MIN as f32, u8::MAX as f32) as u8])
